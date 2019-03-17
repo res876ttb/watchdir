9aefa369e207c3929fcf22bb3b343c9ab160decf
res876ttb <someone10851222@gmail.com>
2019-03-17 20:47:28 +0800

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Watchdir
+
+Watch if anything in a directory is changed.
+
+## Dependency
+
+```shell
+pip install watchdog
+```
+
+## Usage
+
+```shell
+./watchdir.py /path/to/directory "command"
+```
+
+\*command should be quoted by ""
---
+++ watchdir.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os, sys
+import time
+import argparse
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dir', help='The directory you want to observe.')
+parser.add_argument('command', help='The command you want to run whenever something is changed in the directory you watch.')
+args = parser.parse_args()
+
+# global variable
+command = args.command.replace("'", '').replace('"', '')
+
+# class
+class directoryEvent(LoggingEventHandler):
+  def __init__(self):
+    super(directoryEvent, self).__init__()
+
+  def on_any_event(self, event):
+    super(directoryEvent, self).on_any_event(event)
+    print('%s is changed in directory %s' % (event.src_path, args.dir))
+    print('Run command: %s' % args.command)
+    os.system('%s' % command)
+
+observer = Observer()
+eventHandler = directoryEvent()
+observer.schedule(eventHandler, args.dir, recursive=True)
+observer.start()
+
+try:
+  while True:
+    time.sleep(1)
+except KeyboardInterrupt:
+  observer.stop()
+
+observer.join()
+
